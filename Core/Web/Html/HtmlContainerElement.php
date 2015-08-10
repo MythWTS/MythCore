1c5ac44e21e51fe1914142a76a7e644f36b5d40d
mythwts <mythwts@gmail.com>
2015-08-11 03:11:06 +0800

    - fixed a mistake in HtmlEmptyElement - added some code to HtmlContainerElement and HtmlEmptyElement including overrides.

--- Core/Web/Html/HtmlContainerElement.php
+++ Core/Web/Html/HtmlContainerElement.php
@@ -1,5 +1,20 @@
 <?php
 class HtmlContainerElement extends HtmlElement{
-	
+	###########################################################################
+	# Constructor
+	###########################################################################
+	public function __construct($tag, array $attributes = null, $contents=null, $id='', $class='', $title='', $style='', $indentContents=true){
+		parent::__construct($tag, $attributes, $contents, $id, $class, $title, $style, $indentContents, false);
+	}
+	###########################################################################
+	# Property Accessors Overrides
+	###########################################################################
+	public function IsEmpty(){return false;}
+	###########################################################################
+	# Public Functions
+	###########################################################################
+	public function HasContents(){
+		return is_array($this->_contents) && count($this->_contents) > 0;
+	}
 };
 ?>
--- Core/Web/Html/HtmlEmptyElement.php
+++ Core/Web/Html/HtmlEmptyElement.php
@@ -1,7 +1,20 @@
 <?php
 class HtmlEmptyElement extends HtmlElement{
+	###########################################################################
+	# Constructor
+	###########################################################################
 	public function __construct($tag, array $attributes = null, $id='', $class='', $title='', $style=''){
-		parent::__construct($tag, $attributes, $contents, $id, $class, $title, $style, false, true);
+		parent::__construct($tag, $attributes, null, $id, $class, $title, $style, false, true);
+	}
+	###########################################################################
+	# Property Accessors Overrides
+	###########################################################################
+	public function IsEmpty(){return true;}
+	public function ChildNodes(array $value){
+		if($value === null){return null;}
+	}
+	public function IndentContent($value=null){
+		if($value === null){return false;}
 	}
 };
 ?>
