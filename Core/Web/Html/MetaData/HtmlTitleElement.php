98c7020002fd46fdf88afd2a89db50901d8dffe3
mythwts <mythwts@gmail.com>
2015-08-14 05:15:41 +0800

    made $content optional in the constructor and changed the Title property into PageTitle as it was overriding the Title property from HtmlElement

--- Core/Web/Html/MetaData/HtmlTitleElement.php
+++ Core/Web/Html/MetaData/HtmlTitleElement.php
@@ -1,11 +1,11 @@
 <?php
 class HtmlTitleElement extends HtmlElement implements IDOMMetaData{
-	/** Constructor($pageTitle, $id='') */
-	public function __construct($pageTitle, $id=''){
+	/** Constructor($pageTitle='', $id='') */
+	public function __construct($pageTitle='', $id=''){
 		$content = new HtmlRawTextNode($pageTitle, true);
 		parent::__construct(Html5Tags::$TITLE, null, $content, $id, '', '', '', false);
 	}
-	public function Title($value=null){
+	public function PageTitle($value=null){
 		if($value===null){return $this->_contents[0]->Html;}
 		else{$this->_contents[0] = new HtmlRawTextNode($value);}
 	}
