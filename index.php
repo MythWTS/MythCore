2e2d963026912907c1a5586be8ae6acdfed973aa
mythwts <mythwts@gmail.com>
2015-08-13 12:34:42 +0800

    clearing index and testing rebranching

--- index.php
+++ index.php
@@ -5,16 +5,4 @@ require_once "Core.inc";
  * MOST OF THE TIME THE CONTENTS OF THIS FILE WILL NOT MAKE ANY SENSE, THIS SHOULD CHANGE AT THE ALPHA RELEASE
  */
 ################################################################################################################
-$tst = new HtmlBodyElement();
-$tst->AddNode($n = new HtmlArticleElement("Something"));
-$n->AddSection($d = Html::Dialog());
-$d->AddNode($h = Html::Header());
-$h->AddH1("Heading text");
-$d->AddNode($m = Html::Main());
-$m->AddP("Some body text");
-$d->AddNode($f = Html::Footer());
-$f->AddNode($a = Html::A(Html::H2('heading in footer')));
-$a->Href = 'http:://localhost';
-$d->SetAttribute('open', 'open');
-echo $tst;
 ?>
