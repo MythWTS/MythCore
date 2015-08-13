d748503e34e2f4a5dbda082f1e3165212ec18b95
mythwts <mythwts@gmail.com>
2015-08-14 03:43:09 +0800

    Added class HtmlHtmlElement which represents an html element in the DOM. Since it's always that we need a head and a body element inside the html element, we have them built in as fields of the class that are guarnteed to be created at construction! I'm not sure if that's the case currently, but it is supposed to be impossible to remove them, as the setters already can not accept null as values since they are actually methods! To be seen later.

---
+++ Core/Web/Html/Doc/HtmlHtmlElement.php
@@ -0,0 +1,21 @@
+<?php
+class HtmlHtmlElement extends HtmlGenericContainerElement{
+	protected $_head, $_body;
+	/** Constructor() */
+	public function __construct(HtmlHeadElement $headElement=null, HtmlBodyElement $bodyElement=null){
+		$this->_head = $headElement; $this->_body = $bodyElement;
+		if($this->_head == null){$this->_head = new HtmlHeadElement();}
+		if($this->_body == null){$this->_body = new HtmlBodyElement();}
+		parent::__construct(Html5Tags::$HTML, null, array($this->_head, $this->_body));
+	}
+	
+	public function Head(HtmlHeadElement $value=null){
+		if($value === null){return $this->_head;}
+		else{$this->_head = $value;}
+	}
+	public function Body(HtmlBodyElement $value=null){
+		if($value === null){return $this->_body;}
+		else{$this->_body = $value;}
+	}
+};
+?>
