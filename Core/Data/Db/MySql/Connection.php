39aea689463a257ceb915c647a4bbe041c3d9e73
mythwts <mythwts@gmail.com>
2015-08-15 18:11:58 +0800

    adding skeleton file for _DBResults implementation class

--- Core/Data/Db/MySql/Connection.php
+++ Core/Data/Db/MySql/Connection.php
@@ -2,10 +2,17 @@
 namespace Core\Data\Db\MySql{
 	class Connection extends \Core\Data\Db\ConnectionBase{
 		protected $_conn;
+		############################################################################
+		# Constructor
+		############################################################################
+		/** Constructor($hostName='', $dbName='', $userName='', $userPass='') */
 		public function __construct($hostName='', $dbName='', $userName='', $userPass=''){
 			parent::__construct($hostName, $dbName, $userName, $userPass);
 			$this->_conn = new \mysqli($this->_host, $this->_db, $this->_user, $this->_pass);
 		}
+		############################################################################
+		# IConnection Implementation
+		############################################################################
 		public function Open(){}
 		public function Close(){}
 		public function IsOpen(){}
