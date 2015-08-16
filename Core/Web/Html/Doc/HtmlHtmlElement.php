92fd2b5152aa9597b0ec6eba225b3fc85ec5ee63
mythwts <mythwts@gmail.com>
2015-08-16 22:53:31 +0800

    Modified HtmlHtmlElement constructor to accepts a new argument $indentContents

--- Core/Web/Html/Doc/HtmlHtmlElement.php
+++ Core/Web/Html/Doc/HtmlHtmlElement.php
@@ -2,11 +2,11 @@
 class HtmlHtmlElement extends HtmlGenericContainerElement{
 	protected $_head, $_body;
 	/** Constructor() */
-	public function __construct(HtmlHeadElement $headElement=null, HtmlBodyElement $bodyElement=null){
+	public function __construct(HtmlHeadElement $headElement=null, HtmlBodyElement $bodyElement=null, $indentContents=true){
 		$this->_head = $headElement; $this->_body = $bodyElement;
 		if($this->_head == null){$this->_head = new HtmlHeadElement();}
 		if($this->_body == null){$this->_body = new HtmlBodyElement();}
-		parent::__construct(Html5Tags::$HTML, null, array($this->_head, $this->_body));
+		parent::__construct(Html5Tags::$HTML, null, array($this->_head, $this->_body), '', '', '', '', $indentContents);
 	}
 	
 	public function Head(HtmlHeadElement $value=null){
