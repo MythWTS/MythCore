bc94988ed720da6969384d525fc42e481c9d87d9
mythwts <mythwts@gmail.com>
2015-08-16 22:54:58 +0800

    Made HtmlDocument a child of HtmlDocumentBase and modified the implementation accordingly

--- Core/Web/Html/HtmlDocument.php
+++ Core/Web/Html/HtmlDocument.php
@@ -1,17 +1,8 @@
 <?php
-class HtmlDocument extends HtmlNode{
-	protected $_doctype, $_html, $_indentContents;
+class HtmlDocument extends HtmlDocumentBase{
+	/** Constructor(HtmlHeadElement $head=null, HtmlBodyElement $body=null, HtmlDocTypes $docType=null, $indentContents=true) */
 	public function __construct(HtmlHeadElement $head=null, HtmlBodyElement $body=null, HtmlDocTypes $docType=null, $indentContents=true){
-		$this->_doctype = new HtmlDocTypeNode($docType);
-		$this->_html = new HtmlHtmlElement($head, $body);
-		$this->_indentContents = $indentContents;
-	}
-	public function Render(IWriter $out=null, $indent=0){
-		if($out==null){echo $this->OuterHtml($indent);}
-		else{$out->Write($this->OuterHtml($indent));}
-	}
-	public function OuterHtml($indent=0){
-		return $this->_doctype->OuterHtml() . ($this->_indentContents?U::$NL:'') . $this->_html->OuterHtml($indent);
+		parent::__construct($head, $body, $docType, $indentContents);
 	}
 	
 	public function Head(HtmlHeadElement $value=null){
