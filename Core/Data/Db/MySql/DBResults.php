c4d6aa303141e913cbd8624a0b35d1db8fff3b16
mythwts <mythwts@gmail.com>
2015-08-18 06:15:51 +0800

    Renamed _DBResults to DBResults. Might be useful as a non internal class for users used to the mysqli extension

--- Core/Data/Db/MySql/DBResults.php
+++ Core/Data/Db/MySql/DBResults.php
@@ -1,6 +1,6 @@
 <?php
 namespace Core\Data\Db\MySql{
-	class _DBReults extends \Core\Data\Db\QueryResultsBase{
+	class DBReults extends \Core\Data\Db\QueryResultsBase{
 		/** Constructor(mysqli_result $result) */
 		public function __construct($result){
 			
