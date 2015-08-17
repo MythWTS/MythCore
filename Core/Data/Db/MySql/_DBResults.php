fe34873cbd41d4dcfd265ed24d09fb7db79d1b20
mythwts <mythwts@gmail.com>
2015-08-18 04:26:49 +0800

    Changed constructor to take a generic mixed var then the _DBResults will determine what kind of result it is.

--- Core/Data/Db/MySql/_DBResults.php
+++ Core/Data/Db/MySql/_DBResults.php
@@ -2,7 +2,7 @@
 namespace Core\Data\Db\MySql{
 	class _DBReults extends \Core\Data\Db\QueryResultsBase{
 		/** Constructor(mysqli_result $result) */
-		public function __construct(\mysqli_result $result){
+		public function __construct($result){
 			
 		}
 	};
