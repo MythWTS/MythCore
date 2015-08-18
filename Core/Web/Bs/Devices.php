0790386c9e5005cfc9c5cde8ce7bd3b528dd23bc
mythwtsadmin <mythwts@gmail.com>
2015-08-19 06:33:35 +0800

    Renamed Sizes to Devices

--- Core/Web/Bs/Devices.php
+++ Core/Web/Bs/Devices.php
@@ -1,7 +1,7 @@
 <?php
 namespace Core\Web\Bs;
-class Sizes extends \Enum{
-	public static $XS, $SM, $MD, $LG, $XSmall, $Small, $Medium, $Large;
+class Devices extends \Enum{
+	public static $XS, $SM, $MD, $LG, $XSmall, $Small, $Medium, $Large, $Mobile, $Tablet, $Laptop, $Desktop;
 	protected function __construct($name, $value){parent::__construct($name, $value);}
 	public static function Initialize(){
 		self::$XS = new Sizes('XS', 'xs');
@@ -12,6 +12,10 @@ class Sizes extends \Enum{
 		self::$Small = new Sizes('Small', 'sm');
 		self::$Medium = new Sizes('Medium', 'md');
 		self::$Large = new Sizes('Large', 'lg');
+		self::$Mobile = new Sizes('Mobile', 'xs');
+		self::$Tablet = new Sizes('Tablet', 'sm');
+		self::$Laptop = new Sizes('Laptop', 'md');
+		self::$Desktop = new Sizes('Desktop', 'lg');
 	}
 };
 Sizes::Initialize();
