4ace1715b1336f0bdb37631dd8678b2461799f15
mythwtsadmin <mythwts@gmail.com>
2015-08-19 03:12:02 +0800

    Added class FluidContainer representing an html element that always has the 'container-fluid' class added.

---
+++ Core/Web/Bs/FluidContainer.php
@@ -0,0 +1,9 @@
+<?php
+namespace \Core\Web\Bs;
+class FluidContainer extends \HtmlContainerElement{
+	/** Constructor($tag, $contents='', $id='', $classes='', $title='', $style='', $indentContents=true) */
+	public function __construct($tag, $contents='', $id='', $classes='', $title='', $style='', $indentContents=true){
+		parent::__construct(array('container-fluid'), $tag, $contents, $id, $class, $title, $style, $indentContents);
+	}
+};
+?>
