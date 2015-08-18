a4c0ba2000184da799a5b99604e10b45f3ad81e3
mythwts <mythwts@gmail.com>
2015-08-18 14:06:03 +0800

    Added IsSuccess to IQueryResults and QueryResultsBase, and changed implementation of IsScalar slightly

--- Core/Data/Db/Base/IQueryResults.php
+++ Core/Data/Db/Base/IQueryResults.php
@@ -3,6 +3,7 @@ namespace Core\Data\Db{
 	interface IQueryResults extends \IObject{
 		function IsRowset();
 		function IsScalar();
+		function IsSuccess();
 		function FieldsCount();
 		function RowsCount();
 		function FieldsInfo();
--- Core/Data/Db/Base/QueryResultsBase.php
+++ Core/Data/Db/Base/QueryResultsBase.php
@@ -1,8 +1,9 @@
 <?php
 namespace Core\Data\Db{
 	abstract class QueryResultsBase extends \Object implements IQueryResults{
-		protected $_isScalar, $_isRowset, $_fieldsCount, $_rowsCount, $_fieldsInfo;
-		protected function __construct($isScalar=true, $isRowset=false, $fieldsCount=0, $rowsCount=0, $fieldsInfo=null){
+		protected $_isRowset, $_isSuccess, $_fieldsCount, $_rowsCount, $_fieldsInfo;
+		/** Constructor($isSuccess=true, $isRowset=false, $fieldsCount=0, $rowsCount=0, $fieldsInfo=null) */
+		protected function __construct($isSuccess=true, $isRowset=false, $fieldsCount=0, $rowsCount=0, $fieldsInfo=null){
 			$this->_fieldsCount = $fieldsCount; $this->_rowsCount = $rowsCount;
 			$this->_isRowset = $isRowset; $this->_isScalar = $isScalar;
 			$this->_fieldsInfo = $fieldsInfo;
@@ -11,7 +12,10 @@ namespace Core\Data\Db{
 			return $this->_isRowset;
 		}
 		public function IsScalar(){
-			return $this->_isScalar;
+			return !$this->_isRowset;
+		}
+		public function IsSuccess(){
+			return $this->_isSuccess;
 		}
 		public function FieldsCount(){
 			return $this->_fieldsCount;
