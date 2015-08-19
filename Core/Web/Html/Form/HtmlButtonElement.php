cb7ff62b7325e7a0d3a445b308a060d3071a17d6
mythwtsadmin <mythwts@gmail.com>
2015-08-20 07:46:36 +0800

    Added the name attribute accessor to class HtmlButtonElement

--- Core/Web/Html/Form/HtmlButtonElement.php
+++ Core/Web/Html/Form/HtmlButtonElement.php
@@ -3,6 +3,11 @@ class HtmlButtonElement extends HtmlContainerElement{
 	/** Constructor($content=''='', $id='', $class='', $title='', $style='', $indentContent=false) */
 	public function __construct($content='', $id='', $class='', $title='', $style='', $indentContent=false){
 		parent::__construct(Html5Tags::$BUTTON, null, $content, $id, $class, $title, $style, $indentContent);
+		if(!U::NA($id)){$this->_attributes['name']=$id;}
+	}
+	public function Name($value=null){
+		if($value===null){return $this->_attributes['name'];}
+		else{$this->_attributes['name'] = U::ES($value);}
 	}
 };
 ?>
