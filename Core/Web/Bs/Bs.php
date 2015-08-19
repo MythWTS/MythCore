e07131f5aaef0c105b68c665014b48142bdde96a
mythwtsadmin <mythwts@gmail.com>
2015-08-20 00:59:36 +0800

    minor code modifications + added TextField factory method

--- Core/Web/Bs/Bs.php
+++ Core/Web/Bs/Bs.php
@@ -1,5 +1,5 @@
 <?php
-use Core\Web\Bs as B;
+use B as B;
 final class Bs extends Object{
 	private function __construct(){}
 	###########################################################################
@@ -7,102 +7,102 @@ final class Bs extends Object{
 	###########################################################################
 	# Containers
 	######################################
-	/** Creates a new Core\Web\Bs\Container Object  */
+	/** Creates a new B\Container Object  */
 	public static function Container($tag='div', $contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Container($tag, $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Container($tag, $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function ContainerDiv($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Container('div', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Container('div', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function FluidContainer($tag='div', $contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\FluidContainer($tag, $contents, $id, $class, $title, $style, $indentContents);
+		return new B\FluidContainer($tag, $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function FluidContainerDiv($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\FluidContainer('div', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\FluidContainer('div', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	# Rows
 	######################################
 	public static function Row($tag='div', $contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Row($tag, $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Row($tag, $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function RowDiv($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Row('div', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Row('div', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function RowHeader($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Row('header', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Row('header', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function RowFooter($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Row('footer', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Row('footer', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function RowSection($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Row('section', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Row('section', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	# Columns
 	######################################
 	public static function Column($tag='div', $contents='', $id='', $xsmallWidth=0, $smallWidth=0, $mediumWidth=0, $largeWidth=0, $indentContents=true){
-		return new Core\Web\Bs\Column(
+		return new B\Column(
 			$tag, $contents, $id, 
-			new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, $xsmallWidth),
-			new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$SM, $smallWidth),
-			new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$MD, $mediumWidth),
-			new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$LG, $largeWidth),
+			new B\ColumnDeviceOptions(B\Devices::$XS, $xsmallWidth),
+			new B\ColumnDeviceOptions(B\Devices::$SM, $smallWidth),
+			new B\ColumnDeviceOptions(B\Devices::$MD, $mediumWidth),
+			new B\ColumnDeviceOptions(B\Devices::$LG, $largeWidth),
 			$indentContents
 		);
 	}
 	######################################
 	public static function ColumnFull($tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, null, null, null, null, $indentContents);
+		return new B\Column($tag, $contents, $id, null, null, null, null, $indentContents);
 	}
 	public static function ColumnHalf($tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, 6));
+		return new B\Column($tag, $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, 6));
 	}
 	public static function ColumnThird($tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, 4));
+		return new B\Column($tag, $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, 4));
 	}
 	public static function ColumnQuarter($tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, 3));
+		return new B\Column($tag, $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, 3));
 	}
 	public static function ColumnSixeth($tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, 2));
+		return new B\Column($tag, $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, 2));
 	}
 	######################################
 	public static function ColumnXS($width=1, $tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, $width));
+		return new B\Column($tag, $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, $width));
 	}
 	public static function ColumnSM($width=1, $tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$SM, $width));
+		return new B\Column($tag, $contents, $id, null, new B\ColumnDeviceOptions(B\Devices::$SM, $width));
 	}
 	public static function ColumnMD($width=1, $tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, null, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$MD, $width));
+		return new B\Column($tag, $contents, $id, null, null, new B\ColumnDeviceOptions(B\Devices::$MD, $width));
 	}
 	public static function ColumnLG($width=1, $tag='div', $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column($tag, $contents, $id, null, null, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$LG, $width));
+		return new B\Column($tag, $contents, $id, null, null, null, new B\ColumnDeviceOptions(B\Devices::$LG, $width));
 	}
 	######################################
 	public static function ColumnXSDiv($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('div', $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, $width));
+		return new B\Column('div', $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, $width));
 	}
 	public static function ColumnSMDiv($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('div', $contents, $id, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$SM, $width));
+		return new B\Column('div', $contents, $id, null, new B\ColumnDeviceOptions(B\Devices::$SM, $width));
 	}
 	public static function ColumnMDDiv($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('div', $contents, $id, null, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$MD, $width));
+		return new B\Column('div', $contents, $id, null, null, new B\ColumnDeviceOptions(B\Devices::$MD, $width));
 	}
 	public static function ColumnLGDiv($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('div', $contents, $id, null, null, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$LG, $width));
+		return new B\Column('div', $contents, $id, null, null, null, new B\ColumnDeviceOptions(B\Devices::$LG, $width));
 	}
 	######################################
 	public static function ColumnXSSection($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('section', $contents, $id, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$XS, $width));
+		return new B\Column('section', $contents, $id, new B\ColumnDeviceOptions(B\Devices::$XS, $width));
 	}
 	public static function ColumnSMSection($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('section', $contents, $id, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$SM, $width));
+		return new B\Column('section', $contents, $id, null, new B\ColumnDeviceOptions(B\Devices::$SM, $width));
 	}
 	public static function ColumnMDSection($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('section', $contents, $id, null, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$MD, $width));
+		return new B\Column('section', $contents, $id, null, null, new B\ColumnDeviceOptions(B\Devices::$MD, $width));
 	}
 	public static function ColumnLGSection($width=1, $contents='', $id='', $indentContents=true){
-		return new Core\Web\Bs\Column('section', $contents, $id, null, null, null, new Core\Web\Bs\ColumnDeviceOptions(Core\Web\Bs\Devices::$LG, $width));
+		return new B\Column('section', $contents, $id, null, null, null, new B\ColumnDeviceOptions(B\Devices::$LG, $width));
 	}
 	###########################################################################
 	# Simple Text & Formatting Elements
@@ -130,10 +130,10 @@ final class Bs extends Object{
 	# Formatting
 	######################################
 	public static function Lead($tag='p', $contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Lead($tag, $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Lead($tag, $contents, $id, $class, $title, $style, $indentContents);
 	}
 	public static function LeadP($contents='', $id='', $class='', $title='', $style='', $indentContents=true){
-		return new Core\Web\Bs\Lead('p', $contents, $id, $class, $title, $style, $indentContents);
+		return new B\Lead('p', $contents, $id, $class, $title, $style, $indentContents);
 	}
 	######################################
 	public static function Highlight($content='', $id='', $class='', $title='', $style='', $indentContents=false){
@@ -177,43 +177,47 @@ final class Bs extends Object{
 	# Alignment
 	######################################
 	public static function LeftAligned($tag='p', $content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\LeftAligned($tag, $content, $id, $class, $title, $style, $indentContents);
+		return new B\LeftAligned($tag, $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function RightAligned($tag='p', $content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\RightAligned($tag, $content, $id, $class, $title, $style, $indentContents);
+		return new B\RightAligned($tag, $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function CenterAligned($tag='p', $content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\CenterAligned($tag, $content, $id, $class, $title, $style, $indentContents);
+		return new B\CenterAligned($tag, $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function Justify($tag='p', $content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\JustifyAligned($tag, $content, $id, $class, $title, $style, $indentContents);
+		return new B\JustifyAligned($tag, $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function NoWrap($tag='p', $content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\NoWrapAligned($tag, $content, $id, $class, $title, $style, $indentContents);
+		return new B\NoWrapAligned($tag, $content, $id, $class, $title, $style, $indentContents);
 	}
 	######################################
 	public static function LeftAlignedP($content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\LeftAligned('p', $content, $id, $class, $title, $style, $indentContents);
+		return new B\LeftAligned('p', $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function RightAlignedP($content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\RightAligned('p', $content, $id, $class, $title, $style, $indentContents);
+		return new B\RightAligned('p', $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function CenterAlignedP($content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\CenterAligned('p', $content, $id, $class, $title, $style, $indentContents);
+		return new B\CenterAligned('p', $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function JustifyP($content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\JustifyAligned('p', $content, $id, $class, $title, $style, $indentContents);
+		return new B\JustifyAligned('p', $content, $id, $class, $title, $style, $indentContents);
 	}
 	public static function NoWrapP($content = '', $id = '', $class = '', $title = '', $style = '', $indentContents = false){
-		return new Core\Web\Bs\NoWrapAligned('p', $content, $id, $class, $title, $style, $indentContents);
+		return new B\NoWrapAligned('p', $content, $id, $class, $title, $style, $indentContents);
 	}
-	######################################
-	
 	###########################################################################
 	# Table Elements
 	###########################################################################
 	public static function Table($id='', $striped=false, $hovered=false, $condensed=true, $bordered=false, $indentContents=true){
-		return new Core\Web\Bs\Table($id, $striped, $hovered, $condensed, $bordered, $indentContents);
+		return new B\Table($id, $striped, $hovered, $condensed, $bordered, $indentContents);
+	}
+	###########################################################################
+	# Form Elements
+	###########################################################################
+	public static function TextField($id, B\TextFieldTypes $type=null, $label='', $initialValue='', $placeholderText='', $rows='', $cols=''){
+		return new B\TextField($id, $type, $label, $initialValue, $placeholderText, $rows, $cols);
 	}
 };
 ?>
