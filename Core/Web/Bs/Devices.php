a5a11fc65f09f3801ad1b27de7432e4d2859a8c8
mythwtsadmin <mythwts@gmail.com>
2015-08-19 08:14:40 +0800

    updated forgotten class name. oops!

--- Core/Web/Bs/Devices.php
+++ Core/Web/Bs/Devices.php
@@ -4,19 +4,19 @@ class Devices extends \Enum{
 	public static $XS, $SM, $MD, $LG, $XSmall, $Small, $Medium, $Large, $Mobile, $Tablet, $Laptop, $Desktop;
 	protected function __construct($name, $value){parent::__construct($name, $value);}
 	public static function Initialize(){
-		self::$XS = new Sizes('XS', 'xs');
-		self::$SM = new Sizes('SM', 'sm');
-		self::$MD = new Sizes('MD', 'md');
-		self::$LG = new Sizes('LG', 'lg');
-		self::$XSmall = new Sizes('X-Small', 'xs');
-		self::$Small = new Sizes('Small', 'sm');
-		self::$Medium = new Sizes('Medium', 'md');
-		self::$Large = new Sizes('Large', 'lg');
-		self::$Mobile = new Sizes('Mobile', 'xs');
-		self::$Tablet = new Sizes('Tablet', 'sm');
-		self::$Laptop = new Sizes('Laptop', 'md');
-		self::$Desktop = new Sizes('Desktop', 'lg');
+		self::$XS = new Devices('XS', 'xs');
+		self::$SM = new Devices('SM', 'sm');
+		self::$MD = new Devices('MD', 'md');
+		self::$LG = new Devices('LG', 'lg');
+		self::$XSmall = new Devices('X-Small', 'xs');
+		self::$Small = new Devices('Small', 'sm');
+		self::$Medium = new Devices('Medium', 'md');
+		self::$Large = new Devices('Large', 'lg');
+		self::$Mobile = new Devices('Mobile', 'xs');
+		self::$Tablet = new Devices('Tablet', 'sm');
+		self::$Laptop = new Devices('Laptop', 'md');
+		self::$Desktop = new Devices('Desktop', 'lg');
 	}
 };
-Sizes::Initialize();
+Devices::Initialize();
 ?>
