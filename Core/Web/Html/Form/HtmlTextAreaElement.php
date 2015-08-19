7218d14bcca7e8f33434f3f72000049f4907b982
mythwtsadmin <mythwts@gmail.com>
2015-08-20 03:04:16 +0800

    Added accessor for the name attribute in textarea elements

--- Core/Web/Html/Form/HtmlTextAreaElement.php
+++ Core/Web/Html/Form/HtmlTextAreaElement.php
@@ -4,5 +4,9 @@ class HtmlTextAreaElement extends HtmlContainerElement{
 	public function __construct($content='', $id='', $class='', $title='', $style='', $indentContent=true){
 		parent::__construct(Html5Tags::$TEXTAREA, null, $content, $id, $class, $title, $style, $indentContent);
 	}
+	public function Name($value = null){
+		if($value === null){return $this->_attributes['name'];}
+		else{$this->_attributes['name'] = U::ES($value);}
+	}
 };
 ?>
