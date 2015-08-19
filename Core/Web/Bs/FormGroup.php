1708da93ff4871d760c23b0c56c3bd95340db9cd
mythwtsadmin <mythwts@gmail.com>
2015-08-20 05:47:05 +0800

    Added the ability to make the form group small or large using the bootstrap form-group-sm, form-group-lg classes

--- Core/Web/Bs/FormGroup.php
+++ Core/Web/Bs/FormGroup.php
@@ -1,7 +1,7 @@
 <?php
 namespace Core\Web\Bs;
 class FormGroup extends ClassEnforcedContainerElement{
-	protected $_state;
+	protected $_state, $_lg, $_sm;
 	/** Constructor($contents='', FormState $state=null, $id='', $class='', $title='', $style='', $indentContents=true) */
 	public function __construct($contents='', FormState $state=null, $id='', $class='', $title='', $style='', $indentContents=true){
 		parent::__construct(array('form-group'), 'div', $contents, $id, $class, $title, $style, $indentContents);
@@ -14,6 +14,33 @@ class FormGroup extends ClassEnforcedContainerElement{
 			str_replace($this->_state->__toString(), '', $this->_attributes['class']);
 			$this->_state = $value;
 			$this->_attributes['class'] = trim("{$this->_attributes['class']} {$value->__toString()}");
+		}
+	}
+	
+	public function IsLarge($value=null){
+		if($value === null){return $this->_lg;}
+		else{
+			$value = \U::EB($value);
+			$this->_lg = $value;
+			if($value){
+				if(strpos($this->_attributes['class'], 'form-group-lg') === false){
+					$this->_attributes['class'] = trim("{$this->_attributes['class']} form-group-lg");
+				}
+			}
+			else{str_replace('form-group-lg', '', $this->_attributes['class']);}
+		}
+	}
+	public function IsSmall($value=null){
+		if($value === null){return $this->_sm;}
+		else{
+			$value = \U::EB($value);
+			$this->_sm = $value;
+			if($value){
+				if(strpos($this->_attributes['class'], 'form-group-sm') === false){
+					$this->_attributes['class'] = trim("{$this->_attributes['class']} form-group-sm");
+				}
+			}
+			else{str_replace('form-group-sm', '', $this->_attributes['class']);}
 		}
 	}
 };
