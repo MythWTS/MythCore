3aaaa0f078f1dc109bab8582abc8eed3f4702c85
mythwts <mythwts@gmail.com>
2016-07-27 17:33:48 +0300

    Adding TSingleton, a trait to be used by all singleton classes.

---
+++ Core/TSingleton.php
@@ -0,0 +1,29 @@
+<?php
+namespace Core;
+/**
+ * A trait to be used by static singleton classes (classes that are supposed to have one and only one instance)
+ */
+trait TSingleton{
+	use TObject;
+	/**
+	 * Property method to return the only instance of the singleton
+	 * @return unknown - depends on the using class
+	 */
+	public static function Instance(){
+		static $_instance = null;
+		$class = __CLASS__;
+		return $_instance ?: $_instance = new $class;
+	}
+	/**
+	 * Magic clone method to disallow clonning of the singleton class instance
+	 */
+	public function __clone(){
+		trigger_error('Cloning '.__CLASS__.' is not allowed.',E_USER_ERROR);
+	}
+	/**
+	 * Magic deserialization method to disallow deserializing the sole instance of the class
+	 */
+	public function __wakeup(){
+		trigger_error('Unserializing '.__CLASS__.' is not allowed.',E_USER_ERROR);
+	}
+}
