541fffd023d36ad289765d8c62e06acf395dc6e5
mythwts <mythwts@gmail.com>
2016-07-28 19:18:41 +0300

    Adding Enum, base class for all enumerations

---
+++ Core/Enum.php
@@ -0,0 +1,37 @@
+<?php
+namespace Core;
+/**
+ * Base class for all enumerations in the framework. This class provides the basic utilities for the enumeration classes.
+ * Enumeration classes are almost all declared the same way:
+ * 1) Extend the Enum class.
+ * 2) Define static public fields for the enumeration members.
+ * 3) [Optional] Define a constructor with the same signature as the Enum class constructor that calls the parent constructor.
+ * 4) Define the public static Initialize() method which basically instantiates the fields by creating new objects passing the name value pair to the Enum constructor.
+ * 5) Call the Initialize method immediately after the end of the class declaration
+ */
+class Enum extends Object{
+	protected
+		/** @var string $_name - the name of the enumeration member */
+		$_name,
+		/** @var mixed $_value - the value stored in the enumeration member */
+		$_value;
+	/**
+	 * {@inheritDoc}
+	 * @see \Core\Object::__toString()
+	 */
+	public function __toString(){return $this->_value;}
+	/**
+	 * Protected constructor. Initializes the values for the name and value attributes.
+	 * @param string $name - the name of the enumeration member.
+	 * @param mixed $value - The value associated with the enumeration member. For internal uses mostly.
+	 */
+	protected function __construct($name, $value){$this->_name = $name; $this->_value = $value;}
+	/**
+	 * Getter for the Name property.
+	 */
+	public function Name(){return $this->_name;}
+	/**
+	 * Getter for the Value property
+	 */
+	public function Value(){return $this->_value;}
+}
