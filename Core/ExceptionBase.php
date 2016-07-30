e0b1cb4ce772916dde6ff249127f46c355cb72ad
mythwts <mythwts@gmail.com>
2016-07-30 14:19:46 +0300

    Added a constructor for the ExceptionBase class\

--- Core/ExceptionBase.php
+++ Core/ExceptionBase.php
@@ -1,5 +1,6 @@
 <?php
 namespace Core;
+use \Exception;
 /**
  * ExceptionBase is to serve as the base class for all exceptions in the MythCore framework. Since it's necessary to extend the Exception class for
  * any class to be used with PHP throw statement, this class extends \Exception and implements IObject instead of extending Object like most classes
@@ -13,8 +14,17 @@ namespace Core;
  * @property Exception Previous - the previous exception
  * @property string TraceAsString - a string representation of the stack trace
  */
-class ExceptionBase extends \Exception implements IObject{
+class ExceptionBase extends Exception implements IObject{
 	use TObject;
+	/**
+	 * Creates a new instance of the ExceptionBase class.
+	 * @param string $message - The message of the exception
+	 * @param int $code - the error code associated with the exception
+	 * @param \Exception $previous - the previous exception that was thrown before this one
+	 */
+	public function __construct($message=null, $code=null, $previous=null){
+		parent::__construct($message, $code, $previous);
+	}
 	/**
 	 * Overrides the standard Exception::__toString() method and returns a string that includes most of the details.
 	 * {@inheritDoc}
