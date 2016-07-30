2c90ebe5ff2d3cf3e1b9cd5278a4a937c4a5846d
mythwts <mythwts@gmail.com>
2016-07-30 17:50:31 +0300

    Adding FormatException which is thrown if a format string is not well formed or an invalid format was supplied

---
+++ Core/FormatException.php
@@ -0,0 +1,21 @@
+<?php
+namespace Core;
+/**
+ * Thrown when a format string is not well formatted or the format of a string is not valid in general
+ * @property string Message The exception message
+ * @property int Code The error code associated with this exception
+ * @property string File The filename of the file where the exception occured
+ * @property int Line The line number of the code line where the exception was thrown
+ * @property array Trace An array representing the stack trace
+ * @property Exception Previous The previous exception
+ * @property string TraceAsString A string representation of the stack trace
+ */
+class FormatException extends ExceptionBase{
+	/**
+	 * Creates a new instance of the exception
+	 * @param string $featureName
+	 */
+	public function __construct(){
+		parent::__construct("An invalid format or format string was used", 4);
+	}
+}
