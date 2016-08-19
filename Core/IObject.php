bc94099f1d544bb1c015bc1cb03990da832da256
mythwts <mythwts@gmail.com>
2016-08-19 17:34:50 +0300

    Integrating reflection basic support to the framework by adding the method GetType() to the base interface IObject

--- Core/IObject.php
+++ Core/IObject.php
@@ -21,6 +21,11 @@ interface IObject{
 	 */
 	public function GetClassName();
 	/**
+	 * Returns a Type object containing information about the type of the current object for reflection use.
+	 * @return \Core\Type
+	 */
+	public function GetType();
+	/**
 	 * Returns whether the object is a descendant of the class named $className
 	 * @param string $className The name of the class to check whether this object is a descendant of
 	 * @return bool
