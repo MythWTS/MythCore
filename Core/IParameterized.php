7dffa8f32f08e9e3b062c6b6aa9e77b6065c3e06
mythwts <mythwts@gmail.com>
2016-08-21 07:59:46 +0300

    Adding interface IParameterized which is the base interface for classes that will represent something with an array of parameters

---
+++ Core/IParameterized.php
@@ -0,0 +1,14 @@
+<?php
+namespace Core;
+/**
+ * Interface to be implemented by classes whose instances will represent something that requires parameters (most of the time changeable) to perform their task.
+ * @property array $Parameters An array of parameters to be used for executing a task
+ */
+interface IParameterized extends IObject{
+	/**
+	 * Getter and setter for the Parameters property which sets and gets the parameters used by implementers
+	 * @param array $value used when the method is used as the setter of the property. The array to which to set the property to
+	 * @return array The array of parameters for this instance
+	 */
+	public function &Parameters(array $value = null);
+}
